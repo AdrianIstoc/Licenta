4df9d17f7d60c801450c0fe6bea0d852f4bd567f
AdrianIstoc <adrian.istoc@student.tuiasi.ro>
2026-08-27 18:06:28 +0300

    Monitorizare indici de mediu v1.1; acum in romana

--- App/application.py
+++ App/application.py
@@ -21,7 +21,7 @@ class Application(tk.Tk):
         self.download_option = tk.StringVar(value="None")
         self.prediction_months=tk.IntVar(value=12)
 
-        self.title("Environmental Monitoring")
+        self.title("Monitorizare indici de mediu")
         self.geometry("1280x720")
         self.grid_rowconfigure(0, weight=0)
         self.grid_rowconfigure(1, weight=1)
@@ -65,39 +65,39 @@ class Application(tk.Tk):
         self.status_label.grid(row=2, column=0,columnspan=2, sticky="ew")
 
 
-        self.select_button = tk.Button(self.toolbar, text="Select", command=self.toggle_area_selection)
+        self.select_button = tk.Button(self.toolbar, text="Selectează", command=self.toggle_area_selection)
         self.select_button.pack(side="left")
 
-        self.download_button = tk.Button(self.toolbar, text="Download", command=self.download_selected_data)
+        self.download_button = tk.Button(self.toolbar, text="Descarcă", command=self.download_selected_data)
         self.download_button.pack(side="left")
 
-        self.image_button = tk.Button(self.toolbar, text="Image", command=self.show_image)
+        self.image_button = tk.Button(self.toolbar, text="Afișează imagini", command=self.show_image)
         self.image_button.pack(side="left")
 
-        self.graph_button = tk.Button(self.toolbar, text="Graph", command=self.graph_data)
+        self.graph_button = tk.Button(self.toolbar, text="Afișează grafic", command=self.graph_data)
         self.graph_button.pack(side="left")
 
-        self.predict_button = tk.Button(self.toolbar, text="Prediction", command = self.prediction)
+        self.predict_button = tk.Button(self.toolbar, text="Prezice", command = self.prediction)
         self.predict_button.pack(side="left")
 
 
-        self.label_start = tk.Label(self.side_menu, text="Start date")
+        self.label_start = tk.Label(self.side_menu, text="Data de început")
         self.label_start.pack()
 
         self.calendar_start = Calendar(self.side_menu, selectmode="day")
         self.calendar_start.pack()
 
-        self.label_end = tk.Label(self.side_menu, text="End date")
+        self.label_end = tk.Label(self.side_menu, text="Data de final")
         self.label_end.pack()
 
         self.calendar_end = Calendar(self.side_menu, selectmode="day")
         self.calendar_end.pack()
 
-        self.confirm_date_button = tk.Button(self.side_menu, text="Confirm date", command=self.confirmDate)
+        self.confirm_date_button = tk.Button(self.side_menu, text="Confirmă data", command=self.confirmDate)
         self.confirm_date_button.pack()
 
 
-        self.option_label = tk.Label(self.side_menu, text="Download:")
+        self.option_label = tk.Label(self.side_menu, text="Descarcă:")
         self.option_label.pack(pady=(20, 5))
 
         self.rgb_radio = tk.Radiobutton(self.side_menu, text="RGB", variable=self.download_option, value="RGB")
@@ -110,7 +110,7 @@ class Application(tk.Tk):
         self.ndwi_radio.pack(anchor="w")
 
 
-        self.prediction_label=tk.Label(self.side_menu, text="Prediction months:")
+        self.prediction_label=tk.Label(self.side_menu, text="Luni prezise:")
         self.prediction_label.pack(pady=(20, 5))
 
         self.prediction_spinbox=tk.Spinbox(self.side_menu, from_=1, to=60, textvariable=self.prediction_months, width=10)
@@ -118,14 +118,13 @@ class Application(tk.Tk):
 
 
     def prediction(self):
-        if self.downloaded_data is None:
-            self.show_status(text="No data to predict!", duration=3000)
+        if self.downloaded_data is None or self.downloaded_data["results"]==[]:
+            self.show_status(text="Nu există date pentru predicție!", duration=3000)
             return
         elif self.downloaded_data["option"]=="RGB":
-            self.show_status(text="Cannot predict RGB!", duration=3000)
+            self.show_status(text="Nu se pot prezice date RGB!", duration=3000)
             return
-
-
+    
         predictions = predict(results=self.downloaded_data["results"], n=self.prediction_months.get())
 
         dates= [result["date"] for result in predictions]
@@ -134,18 +133,18 @@ class Application(tk.Tk):
         graph_data(dates=dates, indice=indice, indice_name=self.downloaded_data["option"])        
 
     def show_image(self):
-        if self.downloaded_data is None:
-            self.show_status(text="No data to show!", duration=3000)
+        if self.downloaded_data is None or self.downloaded_data["results"]==[]:
+            self.show_status(text="Nu există date!", duration=3000)
             return
 
         plot_image(self.downloaded_data)
 
     def graph_data(self):
-        if self.downloaded_data is None:
-            self.show_status(text="No data to graph!", duration=3000)
+        if self.downloaded_data is None or self.downloaded_data["results"]==[]:
+            self.show_status(text="Nu există date!", duration=3000)
             return
         elif self.downloaded_data["option"] == "RGB":
-            self.show_status(text="Cannot graph RGB!", duration=3000)
+            self.show_status(text="Nu se poate face un grafic pentru RGB!", duration=3000)
             return
 
         dates = [result["date"] for result in self.downloaded_data["results"]]
@@ -156,16 +155,16 @@ class Application(tk.Tk):
 
     def download_selected_data(self):
         if self.download_option.get() == "None":
-            self.show_status(text="No download option selected!", duration=3000)
+            self.show_status(text="Nu s-a ales o opțiune pentru descărcarea datelor!", duration=3000)
             return
         elif self.starting_point is None:
-            self.show_status(text="No area selected!", duration=3000)
+            self.show_status(text="Nu s-a selectat o zonă!", duration=3000)
             return
         elif self.start_date is None:
-            self.show_status(text="No time window selected!", duration=3000)
+            self.show_status(text="Nu s-a selectat o perioadă de timp!", duration=3000)
             return
         elif self.start_date > self.end_date:
-            self.show_status(text="Starting date must be befor the end date!", duration=3000)
+            self.show_status(text="Data de început trebuie să fie înainte de data de final!", duration=3000)
             return
         
         bbox = BBox(bbox=[self.starting_point[1], self.starting_point[0], self.current_point[1], self.current_point[0]], crs=CRS.WGS84)
@@ -178,14 +177,14 @@ class Application(tk.Tk):
             case "NDWI":
                 self.downloaded_data = download_ndwi_data(config=self.config, collection=self.collection, bbox=bbox, start_date=self.start_date, end_date=self.end_date)
 
-
-        self.show_status(text="!!!Data downloaded!!!")
+        print(self.downloaded_data)
+        self.show_status(text="!!!Date descărcate!!!")
 
 
     def confirmDate(self):
         self.start_date = self.calendar_start.selection_get()
         self.end_date = self.calendar_end.selection_get()
-        self.show_status(text="Date selected!", duration=3000)
+        self.show_status(text="Perioadă de timp selectată!", duration=3000)
 
 
 
@@ -194,11 +193,11 @@ class Application(tk.Tk):
         self.selecting_area = not self.selecting_area
 
         if self.selecting_area:
-            self.select_button.config(text="Cancel", relief=tk.SUNKEN)
+            self.select_button.config(text="Nu mai selecta", relief=tk.SUNKEN)
             self.enable_selection()
-            self.show_status("Selection mode: drag with the right mouse button.", duration=3000)
+            self.show_status("Selectează: folosește clic dreapta pentru a selecta!", duration=3000)
         else:
-            self.select_button.config(text="Select", relief=tk.RAISED)
+            self.select_button.config(text="Selectează", relief=tk.RAISED)
             self.disable_selection()
 
     def enable_selection(self):
@@ -248,7 +247,7 @@ class Application(tk.Tk):
         self.draw_selection()
 
         if self.starting_point is not None:
-            self.show_status("Selection locked.", duration=3000)
+            self.show_status("Selecție salvată.", duration=3000)
         
 
     def show_status(self, text, duration=None):
